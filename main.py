2b70db5cc61b9cb075b94ac3defd53d8c51ebde8
vere small <veresmall19@hotmail.com>
2026-08-15 15:32:45 -0400

    Write cleaned data to csv

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ import csv
 
 with open("data/messy_data.csv", newline="") as file:
     reader = csv.reader(file)
+    next(reader)
 
-    for row in reader:
-        print(row)
-        print(row[0])
+    with open("output/cleaned_data.csv", "w", newline="") as output_file:
+        writer = csv.writer(output_file)
+
+        writer.writerow(["Name", "Email", "State"])
+
+        for row in reader:
+            name = row[0].title()
+            email = row[1].lower()
+            state = row[2].upper()
+
+            writer.writerow([name, email, state])
