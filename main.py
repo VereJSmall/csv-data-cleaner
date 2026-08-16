f3598728ee8e96ddd78ce3f7cbd3a39edeaddecd
vere small <veresmall19@hotmail.com>
2026-08-16 03:35:33 -0400

    Fix duplicate filtering logic

--- main.py
+++ main.py
@@ -7,16 +7,14 @@ with open("data/messy_data.csv", newline="") as file:
     with open("output/cleaned_data.csv", "w", newline="") as output_file:
         writer = csv.writer(output_file)
 
-        writer.writerow(["Name", " Email", " State"])
+        writer.writerow(["Name", "Email", "State"])
 
-        seen = set
+        seen = set()
 
         for row in reader:
             name = row[0].title()
             email = row[1].lower()
             state = row[2].upper()
-
-            writer.writerow([name, email, state])
 
             if name not in seen:
                 seen.add(name)
